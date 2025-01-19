686300bd37ecd8d9a02006c4f50ba213171b9930
Dev <devitaliya22@gmail.com>
2025-01-19 23:30:58 +0530

    Refactor : update internal packages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
+import { Button } from "@upcord/ui/button"
 import React from "react";
 import styles from "./page.module.css";
 
